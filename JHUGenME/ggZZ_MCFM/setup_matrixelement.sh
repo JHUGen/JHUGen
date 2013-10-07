49415d3dcc216389e5bca3d16e0f444c2896ee81
Nhan Tran <nhan.v.tran@gmail.com>
2013-10-07 11:56:02 -0500

    change dir name

--- JHUGenME/ggZZ_MCFM/setup_matrixelement.sh
+++
@@ -1,4 +0,0 @@
-. /afs/cern.ch/sw/lcg/contrib/gcc/4.3/x86_64-slc5/setup.sh
-export ROOTSYS=/afs/cern.ch/sw/lcg/app/releases/ROOT/5.32.00/x86_64-slc5-gcc43-opt/root/
-export PATH=$HOME/bin:$ROOTSYS/bin:$PATH
-export LD_LIBRARY_PATH=$ROOTSYS/lib:$LD_LIBRARY_PATH
