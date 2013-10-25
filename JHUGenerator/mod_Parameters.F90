2e15cfb9cb40d9349d38648144020cd7c04cc305
Nhan Tran <nhan.v.tran@gmail.com>
2013-10-25 15:57:47 -0500

    update to 4.1.1

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 ! 
 ! 
-character(len=6),parameter :: JHUGen_Version="v4.0.4"
+character(len=6),parameter :: JHUGen_Version="v4.1.1"
 ! 
 ! 
 integer, public :: Collider, PDFSet,PChannel,Process,DecayMode1,DecayMode2
