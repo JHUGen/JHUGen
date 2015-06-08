bd3f9a0e9c29201d72400c1d4a524d0911f269bd
Markus Schulze <markus.schulze@cern.ch>
2015-06-08 10:33:22 +0200

    Fixed TopDK=0 issue

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 ! 
 ! 
-character(len=6),parameter :: JHUGen_Version="v5.6.0"
+character(len=6),parameter :: JHUGen_Version="v5.6.1"
 ! 
 ! 
 integer, public :: Collider, PDFSet,PChannel,Process,DecayMode1,DecayMode2,TopDecays
