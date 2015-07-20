926272d6145f85b620351ca4eed0bc84194ca7ae
Heshy Roskes <jroskes1@jhu.edu>
2015-07-20 08:58:10 -0400

    Get version automatically in makeTAR.sh

--- Web/Generator/makeTAR.sh
+++ Web/Generator/makeTAR.sh
@@ -1,6 +1,20 @@
-#!bin/bash/
+#!/bin/bash
 
 INDIR="../../"
+
+
+cd $INDIR/JHUGenerator
+make || ( echo "compiling JHUGen failed!"; exit 1 )
+cd - > /dev/null
+cd $INDIR/JHUGenMELA
+make || ( echo "compiling JHUGenMELA failed!"; exit 1 )
+cd - > /dev/null
+
+
+
 VERSION=$1
+if [ ! $VERSION ]; then
+    VERSION=$(grep JHUGen_Version ../../JHUGenerator/mod_Parameters.F90 | grep JHUGen_Version | sed -r 's/.*"(.*)".*/\1/')
+fi
 
 tar -czvf "JHUGenerator."$VERSION".tar.gz" $INDIR"/JHUGenerator" $INDIR"/JHUGenMELA" $INDIR"/AnalyticMELA"
