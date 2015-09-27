1ff447278e53f36959e4bfad5b636a9657fd5761
usarica <ulascan90@gmail.com>
2015-09-27 07:30:24 -0400

    Ga_tau PDG value

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -73,7 +73,7 @@ real(8), public, parameter :: m_el = 0.00051100d0  *GeV         ! electron mass
 real(8), public, parameter :: m_mu = 0.10566d0     *GeV         ! muon mass
 real(8), public, parameter :: m_tau = 1.7768d0     *GeV         ! tau mass
 real(8), public, parameter :: m_bot = 4.75d0       *GeV         ! bottom quark mass
-real(8), public, parameter :: Ga_tau =4d-13        *GeV         ! tau width
+real(8), public, parameter :: Ga_tau =2.267d-12        *GeV         ! tau width
 
 real(8), public, parameter :: HiggsDecayLengthMM = 0d0      ! Higgs decay length in [mm]
 real(8), public, parameter :: Gf = 1.16639d-5/GeV**2        ! Fermi constant
