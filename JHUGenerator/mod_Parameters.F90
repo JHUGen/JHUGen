d8c34a26d8d53e20082810bfe32f77c81901c4b6
Heshy Roskes <jroskes1@jhu.edu>
2015-10-01 10:16:25 -0400

    Version number v6.2.8

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 ! 
 ! 
-character(len=6),parameter :: JHUGen_Version="v6.2.7"
+character(len=6),parameter :: JHUGen_Version="v6.2.8"
 ! 
 ! 
 integer, public :: Collider, PDFSet,PChannel,Process,DecayMode1,DecayMode2,TopDecays
