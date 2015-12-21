2984e61db348fee6dc8c43d1ffc2d2fe90601318
usarica <ulascan90@gmail.com>
2015-12-21 15:28:52 -0500

    Update version number and manual

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 ! 
 ! 
-character(len=6),parameter :: JHUGen_Version="v6.7.7"
+character(len=6),parameter :: JHUGen_Version="v6.7.8"
 ! 
 ! 
 !=====================================================
