673cb490ac220493deabc508a1b12fa541e263f1
usarica <ulascan90@gmail.com>
2016-01-07 14:54:56 -0500

    Compile tex

--- Web/Generator/makeTAR.sh
+++ Web/Generator/makeTAR.sh
@@ -19,5 +19,8 @@ if [ ! $VERSION ]; then
     VERSION=$(grep JHUGen_Version ../../JHUGenerator/mod_Parameters.F90 | grep JHUGen_Version | sed -r 's/.*"(.*)".*/\1/')
 fi
 
+pushd $INDIR"/Manual/"
+pdflatex manJHUGenerator.tex
+popd
 cp $INDIR"/Manual/manJHUGenerator.pdf" "../manJHUGenerator."$VERSION".pdf"
 tar -czvf "JHUGenerator."$VERSION".tar.gz" $INDIR"/JHUGenerator" $INDIR"/JHUGenMELA" $INDIR"/AnalyticMELA" "../manJHUGenerator."$VERSION".pdf"
