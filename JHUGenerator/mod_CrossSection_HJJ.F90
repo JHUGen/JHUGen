394d5f3f26aeccad5662e511103f10a0ea0ac990
Heshy Roskes <jroskes1@jhu.edu>
2016-03-07 18:07:41 -0500

    For now, don't link to MELA by default but add an option to make it easier

--- JHUGenerator/mod_CrossSection_HJJ.F90
+++ JHUGenerator/mod_CrossSection_HJJ.F90
@@ -136,7 +136,13 @@ NumPartonicChannels = 2
 
 
  msq_MCFM(:,:) = 0d0
+#if linkMELA==1
  call qq_ZZqq(p_MCFM,msq_MCFM,HZZcoupl,HWWcoupl,Lambda*100d0,Lambda_Q*100d0,(/Lambda_z1,Lambda_z2,Lambda_z3,Lambda_z4/)*100d0)!  q(-p1)+q(-p2)->Z(p3,p4)+Z(p5,p6)+q(p7)+q(p8)
+#else
+ print *, "To use this process, please set linkMELA=Yes in the makefile and recompile."
+ print *, "You will also need to have a compiled JHUGenMELA in the directory specified by JHUGenMELADir in the makefile."
+ stop 1
+#endif
  ! large overhead here because MCFM computes all partonic channels and we only use msq_MCFM(iPart_sel,jPart_sel)  !
 
 
