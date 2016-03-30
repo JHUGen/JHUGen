155cc03c858711d1ddadd6555fc44b40c261f910
Heshy Roskes <jroskes1@jhu.edu>
2016-03-30 14:16:38 -0400

    remove unused function (gives compiler warning on my new computer)

--- JHUGenerator/genps.h
+++ JHUGenerator/genps.h
@@ -7,7 +7,6 @@
 
 extern void           gensing_(int *, double *, double *, double [][4], int *, int *, double *, int *);
 extern void inline    swapmom_(double *, double *);
-extern void inline    swap_(double*, double*);
 extern void           genps_( int *, double *, double *, double *, double [][4], double *);
 
 static inline void    boostback(double *, double *, double *, double *);
