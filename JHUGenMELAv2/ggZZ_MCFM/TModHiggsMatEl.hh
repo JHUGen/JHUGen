3ae7a482bf516ab94dfe1e3e7ea2d8f85c0f2161
usarica <ulascan90@gmail.com>
2016-06-11 04:18:47 -0400

    Start migrating MELAv2 into JHUGenMELA

---
+++ JHUGenMELAv2/ggZZ_MCFM/TModHiggsMatEl.hh
@@ -0,0 +1,10 @@
+
+#ifndef _TMODHIGGSMATEL_HH_
+#define _TMODHIGGSMATEL_HH_
+
+extern "C" {
+  void __modhiggs_MOD_evalamp_gg_h_vv(double P[6][4], double *MReso,  double *GaReso, double Hggcoupl[3][2], double Hvvcoupl[39][2], int *MYIDUP, double *MatElSq);
+}
+
+#endif
+
