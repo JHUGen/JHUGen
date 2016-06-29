7438dbd1190987814757f571dc5225cb7347b5c6
usarica <ulascan90@gmail.com>
2016-06-29 16:38:17 -0400

    Missing/excess files

--- JHUGenMELAv2/MELA/test/loadMELA.C
+++ JHUGenMELAv2/MELA/test/loadMELA.C
@@ -3,6 +3,6 @@
   gSystem->Load("libPhysics.so");
   gSystem->Load("libEG.so");
   gSystem->AddIncludePath("-I$ROOFITSYS/include/");
-  gSystem->Load("libmcfm_7p0.so");
+  gSystem->Load("libmcfm_701.so");
   gSystem->Load("libME.so");
 }
