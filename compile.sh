51b7d9034368596961c844e92d2a8eda828c8161
Ulascan Sarica <ulascan90@gmail.com>
2016-10-23 08:46:44 +0200

    Fix LambdaBSM vs Lambda2BSM in second resonance gh2g3 coupling, add all initializations to mdata, fix compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-cmsswdir=/work-zfs/lhc/usarica/CMS-related/MELAdevelopment/CMSSW_8_0_12/src
+cmsswdir=../
 scramarch=slc6_amd64_gcc530
 
+for aDir in QCDLoop/ff QCDLoop/ql QCDLoop TensorReduction/ov TensorReduction/pv TensorReduction/ov TensorReduction/recur/smallY TensorReduction/recur/smallP TensorReduction/recur/smallG TensorReduction/recur/smallF TensorReduction/recur
+do
+   pushd $aDir; mkdir -p obj; make; popd
+done
+
+mkdir -p obj
 make
 cd obj
 g++ -Wl,-soname,libmcfm_701.so -shared -o libmcfm_701.so *.o ../QCDLoop/ql/obj/ql*.o ../QCDLoop/ql/obj/a*.o ../QCDLoop/ff/obj/*.o ../TensorReduction/ov/*.a ../TensorReduction/pv/*.a ../TensorReduction/recur/*.a
