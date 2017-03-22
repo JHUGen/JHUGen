ae7cdd8a7641478034d204ef255f74520f2f3d4e
yaofuzhou <einsteiniixy@gmail.com>
2017-03-22 01:25:58 -0400

    ggVH in progress

--- JHUGenerator/mod_VHiggs.F90
+++ JHUGenerator/mod_VHiggs.F90
@@ -696,7 +696,7 @@ complex(8) function MATRIXELEMENT0qq(MomExt,mass,helicity,id)
       if(id(8).ne.Not_a_particle_) then
         PROP5 = -PROPAGATOR(dsqrt(q5_q5),mass(5,1),mass(5,2))
 !print *, id(8), MomExt(:,8), helicity(8), id(9), MomExt(:,9), helicity(9)
-!  print *, FFS(id(8), MomExt(:,8), helicity(8), id(9), MomExt(:,9), helicity(9))
+print *, FFS(id(8), MomExt(:,8), helicity(8), id(9), MomExt(:,9), helicity(9))
         MATRIXELEMENT0gg=MATRIXELEMENT0gg *PROP5 &
         *(kappa*FFS(id(8), MomExt(:,8), helicity(8), id(9), MomExt(:,9), helicity(9)) &
          +kappa_tilde*FFP(id(8), MomExt(:,8), helicity(8), id(9), MomExt(:,9), helicity(9)))&
