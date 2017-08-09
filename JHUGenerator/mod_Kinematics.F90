ab963d6b0c63981dca28c20d98b97bc4732cd318
Markus Christian Schulze <markus.schulze@cern.ch>
2017-08-09 16:51:27 +0200

    Removed buggy k_BreitWigner_Quadr

--- JHUGenerator/mod_Kinematics.F90
+++ JHUGenerator/mod_Kinematics.F90
@@ -5263,7 +5263,8 @@ ELSEIF( iChannel.EQ.5 ) THEN
    if( Emin.gt.M_Reso+2*Ga_Reso ) then
       Jac1 = k_l(xRnd(1),Emin**2,min(Energy**2,Emax**2),s56)
    else
-      Jac1 = k_BreitWigner_Quadr(xRnd(1),M_Reso**2,Ga_Reso,Emin**2,Emax**2,s56)
+!       Jac1 = k_BreitWigner_Quadr(xRnd(1),M_Reso**2,Ga_Reso,Emin**2,Emax**2,s56)
+     Jac1 = k_BreitWigner(xRnd(1),M_Reso**2,Ga_Reso,Emin**2,Emax**2,s56)
    endif  
    
       
