5cf7185bf026c631ff1efc390ac418476a6e05b8
Ulascan Sarica <ulascan90@gmail.com>
2017-08-24 06:57:51 +0200

    Misc.

--- src/Inc/zacouplejk.f
+++ src/Inc/zacouplejk.f
@@ -1,3 +1,5 @@
       double precision q_jk(mxpart,mxpart,nf),
      & l_jk(mxpart,mxpart,nf),r_jk(mxpart,mxpart,nf)
-      common/zacouplejk/q_jk,l_jk,r_jk
+      double precision q_j(mxpart,2),
+     & l_j(mxpart,2),r_j(mxpart,2)
+      common/zacouplejk/q_jk,l_jk,r_jk,q_j,l_j,r_j
--- src/Need/addtempmsqtomsq.f
+++ src/Need/addtempmsqtomsq.f
@@ -4,9 +4,9 @@
       integer ime,jme,it,jt,j
       double precision msq(fn:nf,fn:nf),temp(fn:nf,fn:nf),thescale
       msq(ime,jme)=msq(ime,jme)+temp(it,jt)*thescale
-!      if(temp(it,jt) .ne. 0d0)
-!     & write(6,*) "Adding temp(",it,jt,") = ",temp(it,jt),
-!     & " to msq(",ime,jme,") at j=",j
+      if(temp(it,jt) .ne. 0d0)
+     & write(6,*) "Adding temp(",it,jt,") = ",temp(it,jt),
+     & " to msq(",ime,jme,") at j=",j
       end
       subroutine addtempwtomsq(msq,tempw,ime,jme,it,jt,j,thescale)
       implicit none
@@ -14,7 +14,7 @@
       integer ime,jme,it,jt,j
       double precision msq(fn:nf,fn:nf),tempw(fn:nf,fn:nf),thescale
       msq(ime,jme)=msq(ime,jme)+tempw(it,jt)*thescale
-!      if(tempw(it,jt) .ne. 0d0)
-!     & write(6,*) "Adding tempw(",it,jt,") = ",tempw(it,jt),
-!     & " to msq(",ime,jme,") at j=",j
+      if(tempw(it,jt) .ne. 0d0)
+     & write(6,*) "Adding tempw(",it,jt,") = ",tempw(it,jt),
+     & " to msq(",ime,jme,") at j=",j
       end
