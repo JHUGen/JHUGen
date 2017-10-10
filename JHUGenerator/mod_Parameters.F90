23c5f158f0d73e59da8b44d8e405ed02b104542c
Heshy Roskes <jroskes1@jhu.edu>
2017-10-10 17:25:55 +0200

    increment version number

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.0.9"
+character(len=*),parameter :: JHUGen_Version="v7.0.10"
 !
 !
 !=====================================================
