ed7b849f9af5ed0aa38dffd72e4ed4466e8dcc63
Heshy Roskes <jroskes1@jhu.edu>
2017-11-13 12:43:03 -0500

    COLLIER 1.2

--- lib/downloadcollier.sh
+++ lib/downloadcollier.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 cd $(dirname $0)
-wget http://www.hepforge.org/archive/collier/collier-1.1.tar.gz
-tar -xvzf collier-1.1.tar.gz
-rm collier-1.1.tar.gz
-cd COLLIER-1.1/build
+wget http://www.hepforge.org/archive/collier/collier-1.2.tar.gz
+tar -xvzf collier-1.2.tar.gz
+rm collier-1.2.tar.gz
+cd COLLIER-1.2/build
 cmake ..
 make
