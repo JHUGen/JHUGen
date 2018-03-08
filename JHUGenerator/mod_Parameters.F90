ef3d653eff4c93e7da414ec59c01e801cffeeb3e
Heshy Roskes <jroskes1@jhu.edu>
2018-03-08 13:43:33 -0500

    putting 7.1.3 on the webpage (hidden)

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.1.2"
+character(len=*),parameter :: JHUGen_Version="v7.1.3"
 !
 !
 !=====================================================
