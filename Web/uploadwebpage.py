dc68409b29fa9d996c86ef7ca684e3ba67e6736a
Heshy Roskes <jroskes1@jhu.edu>
2018-03-23 12:31:36 -0400

    making v7.1.4 visible

--- Web/uploadwebpage.py
+++ Web/uploadwebpage.py
@@ -222,8 +222,8 @@ Download_template = """
 #   - melacommit, name of the commit to checkout for the JHUGenMELA folder.  Default is the same
 #       as the generator
 versions = (
+            Version("v7.1.4", manualcommit="e58a25479c1a16b73b657a0a6f660a27be9cd130"),
             Version("v7.1.2"),
-            Version("v7.1.4", visible=False),
             Version("v7.1.0", visible=False),
             Version("v7.0.11", visible=False),
             Version("v7.0.9", manualcommit="c14848e401c115e9e3f201bf7388cc750e4b09f3", melacommit="ebdb109745949008e16851674bb832c59ecc43a3", visible=False),
