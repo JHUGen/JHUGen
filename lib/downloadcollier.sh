f309372aef7875f5490dac9ac78aa963dc846ca4
Heshy Roskes <jroskes1@jhu.edu>
2018-04-25 14:13:42 -0400

    printenv, like in MELA

--- lib/downloadcollier.sh
+++ lib/downloadcollier.sh
@@ -2,15 +2,44 @@
 
 set -euo pipefail
 
+cd $(dirname $0)
+collierversion=1.1
+
+printenv () {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      varname=DYLD_LIBRARY_PATH
+    else
+      varname=LD_LIBRARY_PATH
+    fi
+    echo "export $varname=$(readlink -f COLLIER-$collierversion):$""$varname"
+}
+
+if [ $# -gt 1 ]; then
+  echo "unknown command line arguments"
+  exit 1
+elif [ $# -eq 1 ] && [ $1 == clean ]; then
+  rm -r COLLIER-$collierversion
+  exit
+elif [ $# -eq 1 ] && [ $1 == env ]; then
+  printenv
+  exit
+fi
+
+
 compiler=gfortran  #have to set the SAME compiler in the JHUGen makefile
-if [ $# -ge 1 ]; then
+if [ $# -eq 1 ]; then
   compiler = $1
 fi
 
-cd $(dirname $0)
-wget http://www.hepforge.org/archive/collier/collier-1.1.tar.gz
-tar -xvzf collier-1.1.tar.gz
-rm collier-1.1.tar.gz
-cd COLLIER-1.1/build
+wget http://www.hepforge.org/archive/collier/collier-$collierversion.tar.gz
+tar -xvzf collier-$collierversion.tar.gz
+rm collier-$collierversion.tar.gz
+cd COLLIER-$collierversion/build
 cmake -DCMAKE_Fortran_COMPILER=$compiler ..
 make
+echo
+echo
+echo "Remember to:"
+echo
+printenv
+echo
