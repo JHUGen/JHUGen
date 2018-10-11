fc88a44cac70d486658bfa3ca99c1b1f2cb8cc5c
Heshy Roskes <jroskes1@jhu.edu>
2018-10-11 18:48:12 +0200

    increment version number and replace the one on the webpage

--- JHUGenMELA/MELA/fortran/mod_Parameters.F90
+++ JHUGenMELA/MELA/fortran/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.2.0"
+character(len=*),parameter :: JHUGen_Version="v7.2.1"
 !
 !
 !=====================================================
