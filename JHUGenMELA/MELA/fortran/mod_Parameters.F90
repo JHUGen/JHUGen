44ed7b9e1a414fc9dded404a33718f5b36e63ef0
Heshy Roskes <jroskes1@jhu.edu>
2018-11-15 10:41:11 -0500

    incrementing to v7.2.4

--- JHUGenMELA/MELA/fortran/mod_Parameters.F90
+++ JHUGenMELA/MELA/fortran/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.2.3"
+character(len=*),parameter :: JHUGen_Version="v7.2.4"
 !
 !
 !=====================================================
