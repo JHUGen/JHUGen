4c6d286822d5c85829865fa060f9340eddd5fffb
Heshy Roskes <hroskes@jhu.edu>
2019-01-07 15:39:46 -0500

    first try to test MCFM in circle

--- JHUGenMELA/MELA/setup.sh
+++ JHUGenMELA/MELA/setup.sh
@@ -7,6 +7,7 @@ cd $(dirname $0)
 
 MELADIR="."
 DATA_LIB_DIR="slc6_amd64_gcc530"
+MCFMVERSION=mcfm_705
 
 export SCRAM_ARCH=$DATA_LIB_DIR
 
@@ -25,9 +26,10 @@ printenv () {
     echo "export PYTHONPATH=$(readlink -f $MELADIR)/python$end"
 }
 
-if [[ "$#" -ge 1 ]] && [[ "$1" == "env" ]]; then
+if [[ "$#" -eq 1 ]] && [[ "$1" == "env" ]]; then
     printenv
-elif [[ "$#" -ge 1 ]] && [[ "$1" == *"clean"* ]]; then
+    exit
+elif [[ "$#" -eq 1 ]] && [[ "$1" == *"clean"* ]]; then
     COLLIER/setup.sh "$@"
     make clean
     pushd $MELADIR"/fortran/"
@@ -35,29 +37,37 @@ elif [[ "$#" -ge 1 ]] && [[ "$1" == *"clean"* ]]; then
     rm -f "../data/"$DATA_LIB_DIR"/libjhugenmela.so"
     popd
     make clean
+    exit
+elif [[ "$#" -eq 0 ]]; then
+    : ok
 else
-    COLLIER/setup.sh "$@"
-    tcsh data/retrieve.csh $DATA_LIB_DIR mcfm_705
-    ./downloadNNPDF.sh
-    pushd $MELADIR"/fortran/"
-    make all
-    if mv libjhugenmela.so "../data/"$DATA_LIB_DIR"/"; then
-        echo
-        echo "...and you are running setup.sh, so this was just done."
-        echo
-        popd
-        make
-        echo
-        echo "remember to:"
-        echo
-        printenv
-        echo
-    else
-        echo
-        echo "ERROR: something went wrong in mv, see ^ error message"
-        echo
-        popd
-        exit 1
-    fi
+    echo "Unknown arguments:"
+    echo "  $@"
+    echo "Should be nothing, env, or clean"
+    exit 1
+fi
+
+COLLIER/setup.sh "$@"
+tcsh data/retrieve.csh $DATA_LIB_DIR $MCFMVERSION
+./downloadNNPDF.sh
+pushd $MELADIR"/fortran/"
+make all
+if mv libjhugenmela.so "../data/"$DATA_LIB_DIR"/"; then
+    echo
+    echo "...and you are running setup.sh, so this was just done."
+    echo
+    popd
+    make
+    echo
+    echo "remember to:"
+    echo
+    printenv
+    echo
+else
+    echo
+    echo "ERROR: something went wrong in mv, see ^ error message"
+    echo
+    popd
+    exit 1
 fi
 )
