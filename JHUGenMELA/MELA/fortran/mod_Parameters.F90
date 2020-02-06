63a47898dfc2dfe3672e28dec58e5c9ef2cd4f5c
Ulascan Sarica <ulascan90@gmail.com>
2020-02-05 20:00:37 -0800

    Increment version number

--- JHUGenMELA/MELA/fortran/mod_Parameters.F90
+++ JHUGenMELA/MELA/fortran/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.3.7"
+character(len=*),parameter :: JHUGen_Version="v7.3.8"
 !
 !
 !=====================================================
