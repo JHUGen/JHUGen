f9b05d2bc23bd883a7fc06decb0cc1bfc6f19ede
Ulascan Sarica <ulascan90@gmail.com>
2020-02-05 19:59:53 -0800

    MCFM 7.0.7 update

--- JHUGenMELA/MELA/setup.sh
+++ JHUGenMELA/MELA/setup.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 cd $(dirname $0)
 
 MELADIR="."
-MCFMVERSION=mcfm_706
+MCFMVERSION=mcfm_707
 if [[ -z "${SCRAM_ARCH+x}" ]];then
   export SCRAM_ARCH="slc6_amd64_gcc530"
 fi
--- MCFM-JHUGen/compile.sh
+++ MCFM-JHUGen/compile.sh
@@ -9,7 +9,7 @@ JHUGenbasedir=../
 if [[ -z "${SCRAM_ARCH+x}" ]];then
   export SCRAM_ARCH="slc6_amd64_gcc530"
 fi
-LIB=libmcfm_706.so
+LIB=libmcfm_707.so
 
 for aDir in QCDLoop/ff QCDLoop/ql QCDLoop TensorReduction/ov TensorReduction/pv TensorReduction/ov TensorReduction/recur/smallY TensorReduction/recur/smallP TensorReduction/recur/smallG TensorReduction/recur/smallF TensorReduction/recur
 do
