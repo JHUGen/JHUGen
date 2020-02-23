3b864fb67d336a25076960ff5d5eaad1cc1cf557
Ulascan Sarica <ulascan90@gmail.com>
2020-02-22 19:21:28 -0800

    Incremenet version number to major version

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.3.8"
+character(len=*),parameter :: JHUGen_Version="v7.4.0"
 !
 !
 !=====================================================
