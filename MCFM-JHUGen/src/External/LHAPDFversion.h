d92aefcb80f4a703bae941d4ce21bc6aaab763f2
Ulascan Sarica <ulascan90@gmail.com>
2020-02-25 07:34:13 -0800

    LHAPDF needs the fix from jhugen to be ported

---
+++ MCFM-JHUGen/src/External/LHAPDFversion.h
@@ -0,0 +1,1 @@
+extern "C" void lhapdfversion_(char *version);
--- MCFM-JHUGen/src/Parton/pdfwrap_lhapdf.f
+++ MCFM-JHUGen/src/Parton/pdfwrap_lhapdf.f
@@ -12,54 +12,63 @@
       double precision alphasPDF
       logical validPDF
       character*50 oldPDFname
-      character*72 checkpath 
+      character*72 checkpath
+      character*5 :: LHAPDFversionnumber
       integer i,iorder
 
-      
+
 c      if (newinput .eqv. .false.) then
 c        open(unit=21,file='lhapdf.DAT',status='old',err=999)
 c        call checkversion(21,'lhapdf.DAT')
 c        read(21,*) PDFname
-c        read(21,*) PDFmember            
+c        read(21,*) PDFmember
 c        close(21)
 c      endif
-      
+
       oldPDFname=PDFname
       validPDF=.false.
       i=0
    20 continue
-      i=i+1    
+      i=i+1
       if ((oldPDFname(i:i) .eq. '.') .or.
      .    (oldPDFname(i:i) .eq. ' ') .or.
      .    (oldPDFname(i:i) .eq. '[')) then
         validPDF=.true.
-        if (oldPDFname(i:i+6) .eq. '.LHgrid') then        
+        if (oldPDFname(i:i+6) .eq. '.LHgrid') then
           PDFname=oldPDFname(1:i-1)//'.LHgrid'
         else
           PDFname=oldPDFname(1:i-1)//'.LHpdf'
         endif
-      endif  
+      endif
       if ((i .lt. 40) .and. (validPDF .eqv. .false.)) goto 20
-      
+
       if (validPDF .eqv. .false.) then
         write(6,*) 'Problem with PDFname'
         write(6,*)
         stop
       endif
-      
+
+      call LHAPDFversion(LHAPDFversionnumber)
       write(6,*)
       write(6,*) '*******************************************'
       write(6,*) '*     MCFM is calling LHAPDF              *'
       write(6,*) '*                                         *'
+      write(6,*) '*   LHAPDF version ',LHAPDFversionnumber,
+     . '                  *'
+      write(6,*) '*                                         *'
       write(6,98) 'PDFname',PDFname(1:49)
       write(6,99) 'PDFmember',PDFmember
       write(6,*) '*******************************************'
-      write(6,*)
 
-c      write(6,*) '+ Name = ','PDFsets/'//PDFname
-      call InitPDFset(checkpath('PDFsets/'//PDFname))
+      write(6,*)
+      if (LHAPDFversionnumber .ge. "6.2.1") then
+        call InitPDFSetByName(trim(PDFname))
+      else
+        call InitPDFSet(trim(PDFname))
+      endif
+c      call InitPDFset(checkpath('PDFsets/'//PDFname))
 c      write(6,*) '+ PDF set succesfully initialized'
-      
+
       if (PDFmember .lt. 0) then
         PDFerrors=.true.
         call numberPDF(maxPDFsets)
@@ -68,14 +77,14 @@ c      write(6,*) '+ PDF set succesfully initialized'
           stop
         endif
         write(6,*)
-        write(6,*) '****************************************'        
+        write(6,*) '****************************************'
         write(6,*) '*        Calculating errors using      *'
         write(6,97) maxPDFsets
         write(6,*) '****************************************'
         call InitPDF(0)
         amz=alphasPDF(zmass)
         currentPDF=0
-      else  
+      else
         call InitPDF(PDFmember)
         amz=alphasPDF(zmass)
       endif
@@ -83,12 +92,12 @@ c      write(6,*) '+ PDF set succesfully initialized'
 c--- fill MCFM global variable "nlooprun" with the correct value
       call GetOrderAs(iorder)
       nlooprun=iorder+1
-      
+
 c--- rename pdlabel to get sensible output name
       pdlabel=PDFname(1:7)
 
       return
- 
+
    97 format(' *        ',i4,' sets of error PDFs       *')
    98 format(' *   ',a7,' ',a29,' *')
    99 format(' *  ',a10,i3,'                          *')
@@ -98,5 +107,5 @@ c--- rename pdlabel to get sensible output name
       stop
 
       end
- 
+
 
