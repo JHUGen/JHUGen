b7d0745fb7bae0b147df061b21f8f9e0fb01aa4a
Ulascan Sarica <ulascan90@gmail.com>
2020-03-01 06:55:03 -0800

    Update collier compilation for jhugen

--- lib/downloadcollier.sh
+++ lib/downloadcollier.sh
@@ -3,14 +3,44 @@
 set -euo pipefail
 
 compiler=gfortran  #have to set the SAME compiler in the JHUGen makefile
-if [ $# -ge 1 ]; then
-  compiler = $1
+declare -i ncores
+ncores=0
+
+if [[ "$#" -eq 1 ]] && [[ "$1" == *"clean"* ]]; then
+  rm -rf COLLIER*
+  exit 0
+elif [[ "$#" -eq 1 ]] && [[ "$1" == *"-j"* ]]; then
+  ncores=-1
+elif [[ "$#" -eq 3 ]] && [[ "$1" == *"-j"* ]] && [[ "$2" == *"compiler"* ]]; then
+  ncores=-1
+  compiler=$3
+elif [[ "$#" -eq 3 ]] && [[ "$3" == *"-j"* ]] && [[ "$1" == *"compiler"* ]]; then
+  ncores=-1
+  compiler=$2
+elif [[ "$#" -eq 0 ]]; then
+  : ok
+elif [[ "$#" -eq 2 ]] && [[ "$1" == *"-j"* ]]; then
+  ncores=$2
+elif [[ "$#" -eq 2 ]] && [[ "$1" == *"compiler"* ]]; then
+  compiler=$2
+elif [[ "$#" -eq 4 ]] && [[ "$1" == *"-j"* ]] && [[ "$3" == *"compiler"* ]]; then
+  ncores=$2
+  compiler=$4
+elif [[ "$#" -eq 4 ]] && [[ "$3" == *"-j"* ]] && [[ "$1" == *"compiler"* ]]; then
+  ncores=$4
+  compiler=$2
 fi
 
 cd $(dirname $0)
-wget http://www.hepforge.org/archive/collier/collier-1.2.tar.gz
-tar -xvzf collier-1.2.tar.gz
-rm collier-1.2.tar.gz
+wget http://www.hepforge.org/archive/collier/collier-1.2.0.tar.gz
+tar -xvzf collier-1.2.0.tar.gz
+rm collier-1.2.0.tar.gz
 cd COLLIER-1.2/build
 cmake -DCMAKE_Fortran_COMPILER=$compiler ..
-make
+if [[ $ncores -eq 0 ]];then
+  make
+elif [[ $ncores -eq -1 ]];then
+  make -j
+else
+  make -j $ncores
+fi
