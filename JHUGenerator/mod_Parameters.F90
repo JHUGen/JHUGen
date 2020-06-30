387eb51bc883e7af5d8d7a86d5b3d59513e1835f
Ulascan Sarica <ulascan90@gmail.com>
2020-06-30 14:31:15 -0700

    JHUGen version 7.4.0

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.3.11"
+character(len=*),parameter :: JHUGen_Version="v7.4.0"
 !
 !
 !=====================================================
