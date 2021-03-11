258685b515ec7215227eccb9108f3e1d0f0b32e0
Heshy Roskes <heshyr@gmail.com>
2021-03-11 08:00:44 -0500

    document download_reference_lhes.py

--- .circleci/download_reference_lhes.py
+++ .circleci/download_reference_lhes.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+"""
+This script updates the reference LHE files in reference
+by downloading the LHE files created by circleci in a
+generator_reference job.  This is only needed if the files
+are different, i.e. if the reference_check job fails.
+
+If the build url is
+https://app.circleci.com/pipelines/github/<A>/JHUGen/<B>/workflows/<C>/jobs/<D>
+then you should run
+./download_reference_lhes.py <A> <D>
+
+If the repository is private, you need to provide an access token
+on the command line.  The instructions for creating one are here:
+https://circleci.com/docs/2.0/managing-api-tokens/#creating-a-project-api-token
+It can be done by any member of the JHUGen organization.
+
+After running the script, the LHE files and reference_info.txt
+will be updated.  Commit those changes and push.  The reference_check
+job should then succeed.
+"""
+
 from __future__ import print_function
 
 import argparse, contextlib, os, re, shutil, six
@@ -10,10 +31,10 @@ try:
 except ImportError:
   raise ImportError("Please pip install pyyaml")
 
-p = argparse.ArgumentParser()
-p.add_argument("username", default="JHUGen")
-p.add_argument("build_number", type=int)
-p.add_argument("circleci_access_token", nargs="?")
+p = argparse.ArgumentParser(description=__doc__)
+p.add_argument("username", default="JHUGen", help="circleci username of the person or organization where the tests are run from")
+p.add_argument("build_number", type=int, help="build number of the generator_reference job to get the reference LHEs from")
+p.add_argument("circleci_access_token", nargs="?", help="access token for circleci if the project is private")
 args = p.parse_args()
 
 here = os.path.dirname(__file__)
