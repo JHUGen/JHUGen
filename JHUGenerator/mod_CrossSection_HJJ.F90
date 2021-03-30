056c5807d783f6ee31d889d51a3f67963eda973e
Markus Schulze <markus.schulze@physik.hu-berlin.de>
2021-03-30 21:02:07 +0200

    Changed fudge factor in offshell VBF

--- JHUGenerator/mod_CrossSection_HJJ.F90
+++ JHUGenerator/mod_CrossSection_HJJ.F90
@@ -36,7 +36,7 @@ include 'vegas_common.f'
 include 'maxwt.f'
 EvalWeighted_HJJ_fulldecay = 0d0
 m1ffwgt=1d0;m2ffwgt=1d0;m3ffwgt=1d0
-FudgeFactor=1d0
+FudgeFactor=2d0
 
    if (Process.eq.69) then
       call getRef_MCFM_qqVVqqStrong_Hash(ijSel) ! ijSel is in JHU convention
@@ -204,7 +204,7 @@ FudgeFactor=1d0
 
    originalprobability = msq_MCFM(iPart_sel,jPart_sel)
 
-   if( DecayMode1.eq.DecayMode2 ) FudgeFactor = 0.5d0
+   if( DecayMode1.ne.DecayMode2 ) FudgeFactor = FudgeFactor * 2d0
 
    PreFac = hbarc2XsecUnit * FluxFac * PSWgt * sHatJacobi * m1ffwgt * m2ffwgt * m3ffwgt * FudgeFactor
    msq_MCFM = msq_MCFM * PreFac / (GeV**8)  ! adjust msq_MCFM for GeV units of MCFM mat.el.
