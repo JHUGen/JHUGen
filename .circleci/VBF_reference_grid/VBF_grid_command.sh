2abd08a769f6275816b5b7ba5f30d7a299ccbcf7
jejeffre <jdavi231@jhu.edu>
2022-01-07 00:18:06 +0100

    Pre made VBF grids to speed up circleCi jobs

---
+++ .circleci/VBF_reference_grid/VBF_grid_command.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./JHUGen.py Seed=765543533 Process=69 DecayMode1=0 DecayMode2=0 VegasNc0=50000 PDFSet=3 ReweightInterf=1 mJJcut=30 MPhotonCutoff=4 deltaRcut=0.3 pTjetcut=15 m4l_min=124.99 m4l_max=125.01 etajetcut=6.5 MReso=125 GaReso=0.00407 DataFile=compare_to_reference/VBFoffshell_69.lhe CSmaxFile=VBFoffshell_69/output
