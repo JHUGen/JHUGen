1a13ca0760c3707d0f22f2f230c2935ad939d66a
jejeffre <jdavi231@jhu.edu>
2024-05-28 18:57:39 +0200

    Update JHUGenVersion

--- JHUGenerator/mod_Parameters.F90
+++ JHUGenerator/mod_Parameters.F90
@@ -3,7 +3,7 @@ implicit none
 save
 !
 !
-character(len=*),parameter :: JHUGen_Version="v7.5.4"
+character(len=*),parameter :: JHUGen_Version="v7.5.6"
 !
 !
 !=====================================================
